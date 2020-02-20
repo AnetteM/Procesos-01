e78d1c58a1490b43b33b12221199c8e7c2952ab7
AnetteM <anette27montano10@gmail.com>
2020-02-19 18:43:41 -0600

    Titulo de practica!

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-
+// Practica de git
 
 int main(int argc, char** argv) {
-	cout<<"\n\t Hola Mundo\n";
+	cout << "\n\tProcesos de desarrollo de software :)";
+	cout << "\n\n\t\t Hola Mundo\n";
 }
